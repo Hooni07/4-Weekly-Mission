f737f05ee93541ffe51549b87e3cac2a9ff1328c
Hooni07 <gkxogns21@gmail.com>
2024-04-01 18:53:25 +0900

    chore: change Image Component(Image -> img tag)

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -11,13 +11,7 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({ folder, user, proImg }) => {
   return (
     <div className={styles.profile}>
-      <Image
-        src={proImg}
-        alt="프로필 사진"
-        className={styles.imgProfile}
-        width={60}
-        height={60}
-      />
+      <img src={proImg} alt="프로필 사진" className={styles.imgProfile} />
       <p className={styles.userName}>{user}</p>
       <h1 className={styles.folderName}>{folder}</h1>
     </div>
